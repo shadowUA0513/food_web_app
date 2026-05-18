1abb9599d6de20a50299a5aa35701e82160fe24f
Umarjon <101201795+shadowUA0513@users.noreply.github.com>
2026-05-18 13:49:13 +0500

    feat: update fallback base URL for API configuration

--- src/service/api/env.ts
+++ src/service/api/env.ts
@@ -1,4 +1,4 @@
-const fallbackBaseUrl = "https://api.a-bed.uz";
+const fallbackBaseUrl = "https://dynamicrestaurantmanager-production.up.railway.app";
 
 export const env = {
   baseUrl: import.meta.env.VITE_API_BASE_URL ?? fallbackBaseUrl,
